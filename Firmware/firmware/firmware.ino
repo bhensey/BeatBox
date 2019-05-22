25e0476e971ccc175fb998cce4baaf6dbfe8606a
Bennett Hensey <bennett.hensey@gmail.com>
2019-05-22 09:43:12 -0500

    firmware

--- Firmware/firmware/firmware.ino
+++ Firmware/firmware/firmware.ino
@@ -14,23 +14,26 @@
 #include "AudioSampleMetronome.h"
 
 // GUItool: begin automatically generated code
+AudioPlaySdRaw           playSdRaw3;     //xy=201.00390625,220.00390625
+AudioPlaySdRaw           playSdRaw2;     //xy=210.00390625,150.00390625
+AudioPlaySdRaw           playSdRaw4;     //xy=218.00390625,289.00390625
 AudioPlayMemory          playMem1;       //xy=231.25390625,356.50390434265137
-AudioPlaySdWav           playSdWav3;     //xy=233.25390625,249.50390148162842
-AudioPlaySdWav           playSdWav2;     //xy=234.25391006469727,199.50391674041748
-AudioPlaySdWav           playSdWav4;     //xy=234.2539176940918,301.50392723083496
-AudioPlaySdWav           playSdWav1;     //xy=235.25390625,149.50390625
+AudioPlaySdRaw           playSdRaw1;     //xy=277.00390625,90.00390625
+AudioInputI2S            i2s2;           //xy=361.75389862060547,531.0039129257202
 AudioMixer4              mixer1;         //xy=437.50390625,249.50391578674316
 AudioMixer4              mixer2;         //xy=439.25392150878906,332.50392150878906
+AudioRecordQueue         queue1;         //xy=538.75390625,540.50390625
 AudioMixer4              mixer3;         //xy=583.2538986206055,282.50390625
 AudioOutputI2S           i2s1;           //xy=719.2539520263672,287.5039463043213
-AudioConnection          patchCord1(playMem1, 0, mixer2, 0);
-AudioConnection          patchCord2(playSdWav3, 0, mixer1, 2);
-AudioConnection          patchCord3(playSdWav2, 0, mixer1, 1);
-AudioConnection          patchCord4(playSdWav4, 0, mixer1, 3);
-AudioConnection          patchCord5(playSdWav1, 0, mixer1, 0);
-AudioConnection          patchCord6(mixer1, 0, mixer3, 0);
-AudioConnection          patchCord7(mixer2, 0, mixer3, 1);
-AudioConnection          patchCord8(mixer3, 0, i2s1, 0);
+AudioConnection          patchCord1(playSdRaw3, 0, mixer1, 2);
+AudioConnection          patchCord2(playSdRaw2, 0, mixer1, 1);
+AudioConnection          patchCord3(playSdRaw4, 0, mixer1, 3);
+AudioConnection          patchCord4(playMem1, 0, mixer2, 0);
+AudioConnection          patchCord5(playSdRaw1, 0, mixer1, 0);
+AudioConnection          patchCord6(i2s2, 0, queue1, 0);
+AudioConnection          patchCord7(mixer1, 0, mixer3, 0);
+AudioConnection          patchCord8(mixer2, 0, mixer3, 1);
+AudioConnection          patchCord9(mixer3, 0, i2s1, 0);
 AudioControlSGTL5000     sgtl5000_1;     //xy=252,427
 // GUItool: end automatically generated code
 
