ffa355ee9d238a5ab9e99f744faf091cb84d4147
Ben Levinson <benjaminlevinson2019@u.northwestern.edu>
2019-05-22 09:40:31 -0500

    Start integration of simple record

--- Firmware/firmware/firmware.ino
+++ Firmware/firmware/firmware.ino
@@ -44,6 +44,9 @@ AudioControlSGTL5000     sgtl5000_1;     //xy=252,427
 #define SDCARD_MOSI_PIN  11 // not actually used
 #define SDCARD_SCK_PIN   13 // not actually used
 
+const int myInput = AUDIO_INPUT_LINEIN;
+int mode = 0;
+
 #define MENU_HOME                   0
 #define MENU_SESSION_SEL            1
 #define MENU_SESSION                2
@@ -236,6 +239,7 @@ void setup() {
 
   AudioMemory(60); // Memory for all audio funcitons especially recording buffer
   sgtl5000_1.enable();
+  sgtl5000_1.inputSelect(myInput);
   sgtl5000_1.volume(1);
 
   SPI.setMOSI(SDCARD_MOSI_PIN);
@@ -1302,12 +1306,66 @@ void eraseTrack() {
 
 }
 
-void startRecording() {
-
+void startRecording(File frec) {
+  Serial.println("startRecording");
+  if (SD.exists(frec)) {
+    // The SD library writes new data to the end of the
+    // file, so to start a new recording, the old file
+    // must be deleted before new data is written.
+    SD.remove(frec);
+  }
+  
+  if (frec) {
+//    queue1.begin();
+    mode = 1;
+  }
 }
 
-void stopRecording() {
+void stopRecording(File frec) {
+  Serial.println("stopRecording");
+//  queue1.end();
+//  if (mode == 1) {
+//    while (queue1.available() > 0) {
+//      frec.write((byte*)queue1.readBuffer(), 256);
+//      queue1.freeBuffer();
+//    }
+    frec.close();
+  mode = 0;
+}
 
+
+void continueRecording(File frec) {
+//  if (queue1.available() >= 2) {
+//    byte buffer[512];
+//    // Fetch 2 blocks from the audio library and copy
+//    // into a 512 byte buffer.  The Arduino SD library
+//    // is most efficient when full 512 byte sector size
+//    // writes are used.
+//    memcpy(buffer, queue1.readBuffer(), 256);
+//    queue1.freeBuffer();
+//    memcpy(buffer+256, queue1.readBuffer(), 256);
+//    queue1.freeBuffer();
+//    // write all 512 bytes to the SD card
+//    elapsedMicros usec = 0;
+//    frec.write(buffer, 512);
+//    // Uncomment these lines to see how long SD writes
+//    // are taking.  A pair of audio blocks arrives every
+//    // 5802 microseconds, so hopefully most of the writes
+//    // take well under 5802 us.  Some will take more, as
+//    // the SD library also must write to the FAT tables
+//    // and the SD card controller manages media erase and
+//    // wear leveling.  The queue1 object can buffer
+//    // approximately 301700 us of audio, to allow time
+//    // for occasional high SD card latency, as long as
+//    // the average write time is under 5802 us.
+//    //Serial.print("SD write, us=");
+//    //Serial.println(usec);
+//  }
+}
+
+void deleteRecording(File frec) {
+  Serial.println("deleteRecording");
+  stopRecording(frec);
 }
 
 void enterSetting(int selected_setting) {
